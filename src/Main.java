3da682734eea92dc9d13d0a5a2c6812fe0170e14
NATGF <NATGF@DESKTOP-VD6BCHJ.lan>
2022-05-10 02:28:42 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Meu nome é Natalia e não vou começar com Hello World...hahaha");
+		System.out.println("Tenho bom humor, gosto de musica e de desafios");
+		System.out.println("Até breve");
+	}
+}
